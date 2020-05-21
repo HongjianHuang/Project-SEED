be87035de0c8cee75a97a7b948072ad4327f0f44
HongjianHuang <huanghongjian@live.cn>
2020-05-21 17:05:53 -0400

    add start point and end point

---
+++ Assets/Script/EndPoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+
+
+        if (collider.name == "PlayerFoot")
+        {
+            Debug.Log("You win!");
+        }
+    }
+}
---
+++ Assets/Script/GameMangers.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMangers : MonoBehaviour
+{
+
+    private GameObject startPoint;
+    private GameObject endPoint;
+
+    private GameObject player;
+
+    public void Start()
+    {
+        startPoint = GameObject.Find("Start Point");
+        endPoint = GameObject.Find("End Point");
+        player = GameObject.Find("Player");
+        player.transform.position = startPoint.transform.position;
+
+    }
+    public void backToCheckPoint()
+    {
+
+    }
+    public void restartLevel()
+    {
+
+    }
+    public void backToMainMenu()
+    {
+
+    }
+    public void gameOver()
+    {
+
+    }
+    public void nextScene()
+    {
+
+    }
+    public void LateUpdate()
+    {
+
+    }
+}
