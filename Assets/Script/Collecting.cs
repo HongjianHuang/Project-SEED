f3b9c7c49cd7b760836c2901f63049d1dadc5e57
HongjianHuang <huanghongjian@live.cn>
2020-06-02 14:51:00 -0400

    collecting parts

---
+++ Assets/Script/Collecting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collecting : MonoBehaviour
+{
+    // Start is called before the first frame update
+ 
+    
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "BrokenParts")
+        {
+            Debug.Log("hi");
+            
+            Destroy(other.gameObject);
+        }
+    }
+}
