38db0bef7aac9d55fe3442075fa8c7d8750f3c06
HongjianHuang <huanghongjian@live.cn>
2020-06-03 17:37:57 -0400

    ai init

---
+++ Assets/Script/Enemy/EnemyAI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    public float speed;
+
+    public float neetWayPointDistance;
+
+    private Path path;
+    private int currentWayPoint = 0;
+    private bool reachedEndOfPath; 
+
+    private Seeker seeker;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
