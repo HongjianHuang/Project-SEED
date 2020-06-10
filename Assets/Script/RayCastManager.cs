dc9ed36ad0d7ada91e11bbbd5684a2ac0fbc4f70
HongjianHuang <huanghongjian@live.cn>
2020-06-10 15:47:16 -0400

    ray cast to check walls

---
+++ Assets/Script/RayCastManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastManager : MonoBehaviour
+{
+    
+    public bool ChackRayCast(Rigidbody2D rb, int layerMask)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(rb.position, -Vector2.up, layerMask);
+        return hit.collider != null; 
+    }
+}
