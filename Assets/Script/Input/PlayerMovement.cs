8db13348d620788de58991dbb3b170b95e7db1e2
HongjianHuang <huanghongjian@live.cn>
2020-07-07 16:36:01 -0400

    fix playerMovement class name

--- Assets/Script/Input/PlayerMovement.cs
+++ Assets/Script/Input/PlayerMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-public class Player_Movement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 
 
 {
