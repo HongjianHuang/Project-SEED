8f2926288a46992ad042a9fc56a8a482add36b14
HongjianHuang <huanghongjian@live.cn>
2020-08-10 17:19:18 -0400

    version update

--- Assets/Script/Input/Controls.cs
+++ Assets/Script/Input/Controls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Script/Input/Controls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Controls.inputactions'
 
 using System;
 using System.Collections;
@@ -16,20 +16,20 @@ public class @Controls : IInputActionCollection, IDisposable
     ""maps"": [
         {
             ""name"": ""Player"",
-            ""id"": ""f31e57eb-df68-4272-bdf4-ade25660b0ad"",
+            ""id"": ""8e8ccc6e-03a8-4e22-89e2-a50a682454f4"",
             ""actions"": [
                 {
-                    ""name"": ""Movement"",
+                    ""name"": ""MoveRightLeft"",
                     ""type"": ""PassThrough"",
-                    ""id"": ""c08d28fe-6ca9-4398-bd43-2c8fef7469fd"",
-                    ""expectedControlType"": """",
+                    ""id"": ""fa91c9fb-b871-4224-8ca2-19647873e149"",
+                    ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
                 },
                 {
-                    ""name"": ""Roll"",
-                    ""type"": ""Button"",
-                    ""id"": ""a56dec90-903c-45b6-958a-239b1d7831e1"",
+                    ""name"": ""MoveUpDown"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""835eb8b1-607c-4d32-b173-39baab23b11f"",
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
@@ -37,16 +37,24 @@ public class @Controls : IInputActionCollection, IDisposable
                 {
                     ""name"": ""Jump"",
                     ""type"": ""Button"",
-                    ""id"": ""4a14b65a-4d63-4b64-a586-491b4915f72e"",
+                    ""id"": ""f0ac6e44-0541-4a6c-8377-c6de2d6e3ebb"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Press""
+                },
+                {
+                    ""name"": ""Roll"",
+                    ""type"": ""Button"",
+                    ""id"": ""cf92aa47-fd88-4633-8b04-3130f47e5c24"",
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
                 },
                 {
-                    ""name"": ""Att"",
-                    ""type"": ""PassThrough"",
-                    ""id"": ""98f90ef9-682a-4c3a-8157-431352dd6f78"",
-                    ""expectedControlType"": ""Integer"",
+                    ""name"": ""New action"",
+                    ""type"": ""Button"",
+                    ""id"": ""2b804c88-587d-4ab2-a034-ccb22bf20374"",
+                    ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
                 }
@@ -54,103 +62,132 @@ public class @Controls : IInputActionCollection, IDisposable
             ""bindings"": [
                 {
                     ""name"": """",
-                    ""id"": ""4a37d229-b448-4cf8-882e-b09765ba49e4"",
-                    ""path"": ""<Keyboard>/x"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Roll"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": ""Arrow"",
-                    ""id"": ""6ff9dcb1-06cb-4ded-8b06-877e3a32780c"",
-                    ""path"": ""2DVector"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Movement"",
-                    ""isComposite"": true,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": ""up"",
-                    ""id"": ""dc70d987-43ec-4b80-a7ff-843e1c32d74a"",
-                    ""path"": ""<Keyboard>/upArrow"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Movement"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""down"",
-                    ""id"": ""0b3bb11c-93dc-4b72-9858-abed7347b2ae"",
-                    ""path"": ""<Keyboard>/downArrow"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Movement"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""left"",
-                    ""id"": ""8f526bd8-bf8f-458b-b1b1-f081dad94267"",
-                    ""path"": ""<Keyboard>/leftArrow"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Movement"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""right"",
-                    ""id"": ""a50f4d29-50c4-4290-95aa-6ae2bda4a453"",
-                    ""path"": ""<Keyboard>/rightArrow"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Movement"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""cac27243-a4c0-440f-bef2-2d074a75dc9a"",
+                    ""id"": ""00bfcf56-9b41-4e51-a700-fb7e889cf364"",
                     ""path"": ""<Keyboard>/space"",
                     ""interactions"": """",
                     ""processors"": """",
-                    ""groups"": """",
+                    ""groups"": ""Keyboard & mouse"",
                     ""action"": ""Jump"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 },
                 {
                     ""name"": """",
-                    ""id"": ""b804d077-500e-4d86-927d-e1aa01ed887a"",
-                    ""path"": ""<Keyboard>/a"",
+                    ""id"": ""4eab223e-b787-46c2-9508-3f7878bc06a0"",
+                    ""path"": ""<Keyboard>/x"",
+                    ""interactions"": ""Press"",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & mouse"",
+                    ""action"": ""Roll"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""Right&Left"",
+                    ""id"": ""667c5e14-878d-4441-a913-94cb05a692c3"",
+                    ""path"": ""1DAxis"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
-                    ""action"": ""Att"",
+                    ""action"": ""MoveRightLeft"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""745b1968-4dd7-446f-a945-82a2fc8b0f81"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & mouse"",
+                    ""action"": ""MoveRightLeft"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""8432244b-5ce5-45df-85ba-209d7abc96b3"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & mouse"",
+                    ""action"": ""MoveRightLeft"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""9e37a4a2-e1d2-4dbd-bff7-3624c62e5d51"",
+                    ""path"": """",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""New action"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""Up&Down"",
+                    ""id"": ""32830175-b9c8-46a5-a30e-2cd2bc139a1d"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""MoveUpDown"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""e82f1c62-0107-4b21-8960-4ba487d41cfa"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & mouse"",
+                    ""action"": ""MoveUpDown"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""6b7d3a27-f67a-472c-90a8-11be0bfcf6a6"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & mouse"",
+                    ""action"": ""MoveUpDown"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
                 }
             ]
         }
     ],
-    ""controlSchemes"": []
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard & mouse"",
+            ""bindingGroup"": ""Keyboard & mouse"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
 }");
         // Player
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
-        m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
-        m_Player_Roll = m_Player.FindAction("Roll", throwIfNotFound: true);
+        m_Player_MoveRightLeft = m_Player.FindAction("MoveRightLeft", throwIfNotFound: true);
+        m_Player_MoveUpDown = m_Player.FindAction("MoveUpDown", throwIfNotFound: true);
         m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
-        m_Player_Att = m_Player.FindAction("Att", throwIfNotFound: true);
+        m_Player_Roll = m_Player.FindAction("Roll", throwIfNotFound: true);
+        m_Player_Newaction = m_Player.FindAction("New action", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -200,18 +237,20 @@ public class @Controls : IInputActionCollection, IDisposable
     // Player
     private readonly InputActionMap m_Player;
     private IPlayerActions m_PlayerActionsCallbackInterface;
-    private readonly InputAction m_Player_Movement;
-    private readonly InputAction m_Player_Roll;
+    private readonly InputAction m_Player_MoveRightLeft;
+    private readonly InputAction m_Player_MoveUpDown;
     private readonly InputAction m_Player_Jump;
-    private readonly InputAction m_Player_Att;
+    private readonly InputAction m_Player_Roll;
+    private readonly InputAction m_Player_Newaction;
     public struct PlayerActions
     {
         private @Controls m_Wrapper;
         public PlayerActions(@Controls wrapper) { m_Wrapper = wrapper; }
-        public InputAction @Movement => m_Wrapper.m_Player_Movement;
-        public InputAction @Roll => m_Wrapper.m_Player_Roll;
+        public InputAction @MoveRightLeft => m_Wrapper.m_Player_MoveRightLeft;
+        public InputAction @MoveUpDown => m_Wrapper.m_Player_MoveUpDown;
         public InputAction @Jump => m_Wrapper.m_Player_Jump;
-        public InputAction @Att => m_Wrapper.m_Player_Att;
+        public InputAction @Roll => m_Wrapper.m_Player_Roll;
+        public InputAction @Newaction => m_Wrapper.m_Player_Newaction;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -221,43 +260,59 @@ public class @Controls : IInputActionCollection, IDisposable
         {
             if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
             {
-                @Movement.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
-                @Movement.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
-                @Movement.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
-                @Roll.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRoll;
-                @Roll.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRoll;
-                @Roll.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRoll;
+                @MoveRightLeft.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMoveRightLeft;
+                @MoveRightLeft.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMoveRightLeft;
+                @MoveRightLeft.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMoveRightLeft;
+                @MoveUpDown.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMoveUpDown;
+                @MoveUpDown.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMoveUpDown;
+                @MoveUpDown.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMoveUpDown;
                 @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
                 @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
                 @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
-                @Att.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAtt;
-                @Att.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAtt;
-                @Att.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAtt;
+                @Roll.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRoll;
+                @Roll.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRoll;
+                @Roll.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRoll;
+                @Newaction.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnNewaction;
+                @Newaction.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnNewaction;
+                @Newaction.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnNewaction;
             }
             m_Wrapper.m_PlayerActionsCallbackInterface = instance;
             if (instance != null)
             {
-                @Movement.started += instance.OnMovement;
-                @Movement.performed += instance.OnMovement;
-                @Movement.canceled += instance.OnMovement;
-                @Roll.started += instance.OnRoll;
-                @Roll.performed += instance.OnRoll;
-                @Roll.canceled += instance.OnRoll;
+                @MoveRightLeft.started += instance.OnMoveRightLeft;
+                @MoveRightLeft.performed += instance.OnMoveRightLeft;
+                @MoveRightLeft.canceled += instance.OnMoveRightLeft;
+                @MoveUpDown.started += instance.OnMoveUpDown;
+                @MoveUpDown.performed += instance.OnMoveUpDown;
+                @MoveUpDown.canceled += instance.OnMoveUpDown;
                 @Jump.started += instance.OnJump;
                 @Jump.performed += instance.OnJump;
                 @Jump.canceled += instance.OnJump;
-                @Att.started += instance.OnAtt;
-                @Att.performed += instance.OnAtt;
-                @Att.canceled += instance.OnAtt;
+                @Roll.started += instance.OnRoll;
+                @Roll.performed += instance.OnRoll;
+                @Roll.canceled += instance.OnRoll;
+                @Newaction.started += instance.OnNewaction;
+                @Newaction.performed += instance.OnNewaction;
+                @Newaction.canceled += instance.OnNewaction;
             }
         }
     }
     public PlayerActions @Player => new PlayerActions(this);
+    private int m_KeyboardmouseSchemeIndex = -1;
+    public InputControlScheme KeyboardmouseScheme
+    {
+        get
+        {
+            if (m_KeyboardmouseSchemeIndex == -1) m_KeyboardmouseSchemeIndex = asset.FindControlSchemeIndex("Keyboard & mouse");
+            return asset.controlSchemes[m_KeyboardmouseSchemeIndex];
+        }
+    }
     public interface IPlayerActions
     {
-        void OnMovement(InputAction.CallbackContext context);
-        void OnRoll(InputAction.CallbackContext context);
+        void OnMoveRightLeft(InputAction.CallbackContext context);
+        void OnMoveUpDown(InputAction.CallbackContext context);
         void OnJump(InputAction.CallbackContext context);
-        void OnAtt(InputAction.CallbackContext context);
+        void OnRoll(InputAction.CallbackContext context);
+        void OnNewaction(InputAction.CallbackContext context);
     }
 }
